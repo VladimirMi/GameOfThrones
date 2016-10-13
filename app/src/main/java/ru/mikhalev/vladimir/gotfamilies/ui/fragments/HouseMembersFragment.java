db8df1215b3e4144742d6dd3a58ab14a42c0add5
Vladimir <vladimirm703@gmail.com>
2016-10-13 22:58:22 +0300

    Adding CharacterActivity

--- app/src/main/java/ru/mikhalev/vladimir/gotfamilies/ui/fragments/HouseMembersFragment.java
+++ app/src/main/java/ru/mikhalev/vladimir/gotfamilies/ui/fragments/HouseMembersFragment.java
@@ -1,12 +1,13 @@
 package ru.mikhalev.vladimir.gotfamilies.ui.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.List;
@@ -14,24 +15,24 @@ import java.util.List;
 import ru.mikhalev.vladimir.gotfamilies.R;
 import ru.mikhalev.vladimir.gotfamilies.data.managers.DataManager;
 import ru.mikhalev.vladimir.gotfamilies.data.storage.Character;
+import ru.mikhalev.vladimir.gotfamilies.ui.activities.CharacterActivity;
 import ru.mikhalev.vladimir.gotfamilies.ui.adapters.CharactersAdapter;
-import ru.mikhalev.vladimir.gotfamilies.utils.AppConfig;
 import ru.mikhalev.vladimir.gotfamilies.utils.ConstantManager;
 
 public class HouseMembersFragment extends Fragment {
     /**
-     * The fragment argument representing the section number for this
+     * The fragment argument representing the house id for this
      * fragment.
      */
     private static final String ARG_HOUSE_ID = "house_id";
+
     private static final String TAG = ConstantManager.TAG_PREFIX + "HouseMembersFragment";
 
     public HouseMembersFragment() {
     }
 
     /**
-     * Returns a new instance of this fragment for the given section
-     * number.
+     * Returns a new instance of this fragment for the given house id.
      */
     public static HouseMembersFragment newInstance(int houseId) {
 
@@ -51,10 +52,18 @@ public class HouseMembersFragment extends Fragment {
         ListView listView = (ListView) rootView.findViewById(R.id.characters_list);
 
         int houseId = getArguments().getInt(ARG_HOUSE_ID);
-
-        List<Character> characters = DataManager.getInstance().getCharactersForHouse(houseId);
+        final List<Character> characters = DataManager.getInstance().getCharactersForHouse(houseId);
 
         listView.setAdapter(new CharactersAdapter(characters, houseId));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent startCharacterActivity = new Intent(getContext(), CharacterActivity.class);
+                startCharacterActivity.putExtra(ConstantManager.CHARACTER_ID, characters.get(position).getId());
+                startActivity(startCharacterActivity);
+            }
+        });
         return rootView;
     }
 }
--- app/src/main/java/ru/mikhalev/vladimir/gotfamilies/utils/ConstantManager.java
+++ app/src/main/java/ru/mikhalev/vladimir/gotfamilies/utils/ConstantManager.java
@@ -2,4 +2,5 @@ package ru.mikhalev.vladimir.gotfamilies.utils;
 
 public interface ConstantManager {
     String TAG_PREFIX = "DEV ";
+    String CHARACTER_ID = "CHARACTER_ID";
 }
