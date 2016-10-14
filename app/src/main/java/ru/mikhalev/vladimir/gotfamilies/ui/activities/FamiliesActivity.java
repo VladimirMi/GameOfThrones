77089d1114991592c51082b40f2a128aed5e1458
Vladimir <vladimirm703@gmail.com>
2016-10-14 13:21:54 +0300

    Adding Drawer

--- app/src/main/java/ru/mikhalev/vladimir/gotfamilies/ui/activities/FamiliesActivity.java
+++ app/src/main/java/ru/mikhalev/vladimir/gotfamilies/ui/activities/FamiliesActivity.java
@@ -1,8 +1,13 @@
 package ru.mikhalev.vladimir.gotfamilies.ui.activities;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -11,39 +16,105 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ru.mikhalev.vladimir.gotfamilies.R;
 import ru.mikhalev.vladimir.gotfamilies.ui.adapters.SectionsPagerAdapter;
+import ru.mikhalev.vladimir.gotfamilies.utils.AppConfig;
+import ru.mikhalev.vladimir.gotfamilies.utils.ConstantManager;
 
 public class FamiliesActivity extends BaseActivity {
 
+    private static final String TAG = ConstantManager.TAG_PREFIX + "FamiliesActivity";
     private SectionsPagerAdapter mSectionsPagerAdapter;
-    private ViewPager mViewPager;
+
+    @BindView(R.id.toolbar) Toolbar mToolbar;
+    @BindView(R.id.navigation_drawer) DrawerLayout mNavigationDrawer;
+    @BindView(R.id.navigation_view) NavigationView mNavigationView;
+    @BindView(R.id.container) ViewPager mViewPager;
+    @BindView(R.id.tabs) TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_families);
+        ButterKnife.bind(this);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
+        setupToolbar();
+
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
+        setupDrawer();
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mNavigationDrawer.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void setupToolbar() {
+        setSupportActionBar(mToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(getString(R.string.fio));
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setupDrawer() {
+        List<String> housesNames = new ArrayList<>();
+        for (Integer houseId : AppConfig.houseIds) {
+            housesNames.add(mDataManager.getHouseFromDB(houseId).getName());
+        }
+
+        Menu drawerMenu = mNavigationView.getMenu();
+        for (int i = 0; i < 3; i++) {
+            MenuItem house1 = drawerMenu.getItem(i);
+            house1.setTitle(housesNames.get(i));
+        }
+
+        drawerMenu.getItem(0).setChecked(true);
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                mNavigationDrawer.closeDrawer(GravityCompat.START);
+                switch (item.getItemId()) {
+                    case R.id.house1_menu:
+                        mViewPager.setCurrentItem(0);
+                        return true;
+                    case R.id.house2_menu:
+                        mViewPager.setCurrentItem(1);
+                        return true;
+                    case R.id.house3_menu:
+                        mViewPager.setCurrentItem(2);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
